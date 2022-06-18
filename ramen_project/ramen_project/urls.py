ba794eb7e73e4153e7466efbc6ad8dd23a3ccc02
LJH <dhdbwn904@gmail.com>
2022-06-18 23:06:14 +0900

    홈 화면_1

--- ramen_project/ramen_project/urls.py
+++ ramen_project/ramen_project/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from home.views import home
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    path('', home)
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
