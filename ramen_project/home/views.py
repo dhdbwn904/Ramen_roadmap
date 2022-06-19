572f06fcfcf58c8a15a4380fe47ecc0b33a983e6
LJH <dhdbwn904@gmail.com>
2022-06-19 22:37:51 +0900

    지도 띄우기 중간

--- ramen_project/home/views.py
+++ ramen_project/home/views.py
@@ -1,5 +1,11 @@
+from http.client import ImproperConnectionState
 from django.shortcuts import render
+import folium
+from numpy import size
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    map = folium.Map(location=[37.5562364, 126.9292978], zoom_start=20, width='100%', height='100%', min_zoom=10,)
+    maps = map._repr_html_()
+
+    return render(request, 'home.html', {'map' : maps})
